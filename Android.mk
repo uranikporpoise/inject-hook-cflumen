481f33865464f0f6b5128de6b5c1b3dfaf61edf6
uranikporpoise <51081887+uranikporpoise@users.noreply.github.com>
2019-05-30 17:11:29 -0500

    Revert "Update Android.mk"

--- Android.mk
+++ Android.mk
@@ -2,5 +2,27 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+LOCAL_SRC_FILES:= \
+    libhook/hook.cpp \
+    cflumen/maps.cpp \
+    cflumen/cflumen.cpp \
+    hook_main.cpp
 
+LOCAL_MODULE := cflumen
+#LOCAL_CFLAGS += -DDEBUG
+LOCAL_CFLAGS += -std=c++11
+LOCAL_LDLIBS := -lm -ldl -llog -lGLESv2
 
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES:= \
+    libinject/inject.cpp \
+    inject_main.cpp
+
+LOCAL_MODULE := inject
+#LOCAL_CFLAGS += -DDEBUG
+LOCAL_LDLIBS := -ldl -llog
+
+include $(BUILD_EXECUTABLE)
